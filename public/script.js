0ba6eeb350861421f40c0160dad207518e69fc7d
karlos <carlos.kalifa21@gmail.com>
2024-08-27 18:53:42 -0500

    correcion de form de logica

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ function asignarTextoElemento(elemento,texto){
 function encriptar(){
     var texto = document.querySelector('#cuadro__encriptacion').value;
     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "aiyuyy").replace(/o/gi, "oberfoo").replace(/u/gi, "uninun");
-    document.querySelector("texto").value = textoCifrado;
+    document.querySelector("#texto").value = textoCifrado;
     document.querySelector("#cuadro__encriptacion").value;
     }
     console.log(`el texto encriptado es ${textoCifrado}`);
