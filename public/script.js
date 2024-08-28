8e9a76437041c903413352f3b8f6ceba0e474435
karlos <carlos.kalifa21@gmail.com>
2024-08-27 21:21:04 -0500

    encriptador 80%

--- public/script.js
+++ public/script.js
@@ -1,18 +1,29 @@
-function asignarTextoElemento(elemento,texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
-function encriptar(){
-    var texto = document.querySelector('#cuadro__encriptacion').value;
-    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "aiyuyy").replace(/o/gi, "oberfoo").replace(/u/gi, "uninun");
-    document.querySelector("#texto").value = textoCifrado;
-    document.querySelector("#cuadro__encriptacion").value;
+function encriptar (){
+    var texto = document.querySelector("#texto_imput").value;
+    var textoCifrado = texto.replace(/e/gi, "enen").replace(/i/gi, "imssi").replace(/a/gi, "aia").replace(/o/gi, "obort").replace(/u/gi, "ufuaa");
+    document.querySelector(".text-input-salida").value = textoCifrado;
+    document.querySelector("#texto_imput").value;
     }
-    console.log(`el texto encriptado es ${textoCifrado}`);
 
-function desencriptar(){
+
+    
+var boton1 = document.querySelector("#btn-encriptar"); boton1.onclick = encriptar;
+
+function desencriptar (){
+    var texto = document.querySelector("#texto_imput").value; 
+    var textoCifrado = texto.replace(/enen/gi, "e").replace(/imssi/gi, "i").replace(/aia/gi, "a").replace(/obort/gi, "o").replace(/ufuaa/gi, "u"); 
+    document.querySelector(".text-input-salida").value = textoCifrado; 
+    document.querySelector("#texto_imput").value;
 
 }
 
+var boton2 = document.querySelector("#btn-desencriptar"); boton2.onclick = desencriptar;
 
+function copiar(){
+    navigator.clipboard
+    .readText()
+    .then(
+    (clipText) => (document.querySelector(".editor").innerText += clipText),
+  );
+}
+var boton_copiar = document.querySelector("#btn-copiar"); boton_copiar.onclick = copiar;
